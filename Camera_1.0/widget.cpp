1924a78d84b8e0bf61ac11cdee7a3edecc8d4e2f
huangJiaWei-new <huangjiawei2022222@163.com>
2023-05-05 09:30:38 +0800

    Camera_1.0
    组播功能已经实现，两台主机间可以完成简单的应答操作。

---
+++ Camera_1.0/widget.cpp
@@ -0,0 +1,54 @@
+﻿#include "widget.h"
+#include "ui_widget.h"
+#include "QDebug"
+
+Widget::Widget(QWidget *parent)
+    : QWidget(parent)
+    , ui(new Ui::Widget)
+{
+    ui->setupUi(this);
+}
+
+Widget::~Widget()
+{
+    delete ui;
+}
+
+void Widget::on_but_connect_clicked()
+{
+    /*pUdpCast->SetCastMode(CM_MULTICAST);
+    pUdpCast->SetMultiCastLoop(TRUE);
+    pUdpCast->SetMultiCastTtl(6);
+    bool ret =  pUdpCast->Start(LPCTSTR(L"260.0.0.80"), 6566, false, LPCTSTR(L"0.0.0.0"));
+    if(!ret)
+    {
+        DWORD error = GetLastError();
+        qDebug()<<error;
+    }
+    else
+    {
+       qDebug()<<"888";
+    }*/
+}
+
+void Widget::on_but_send_clicked()
+{
+    /*BYTE data = 1;
+    bool ret = pUdpCast->Send(&data, sizeof(data));
+    if(!ret)
+    {
+      DWORD error = SYS_GetLastError();
+      qDebug()<<error;
+      qDebug()<<"send";
+    }*/
+}
+
+/**
+ * @brief 读取UDP数据报
+ */
+void Widget::on_readyRead_clicked()
+{
+    /*pUdpCast->Stop();
+    qDebug()<<"stop";*/
+}
+
---
+++ Camera_1.0/widget.h
@@ -0,0 +1,30 @@
+﻿#ifndef WIDGET_H
+#define WIDGET_H
+
+#include <QWidget>
+#include "MyUdpMultiCast.h"
+#include "HPSocket.h"
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class Widget; }
+QT_END_NAMESPACE
+
+class Widget : public QWidget
+{
+    Q_OBJECT
+
+public:
+    Widget(QWidget *parent = nullptr);
+    ~Widget();
+
+    //IUdpCast *pUdpCast = HP_Create_UdpCast(new MyUdpMultiCastListener());
+
+private:
+    Ui::Widget *ui;
+
+private slots:
+    void on_but_connect_clicked();
+    void on_but_send_clicked();
+    void on_readyRead_clicked();
+};
+#endif // WIDGET_H
